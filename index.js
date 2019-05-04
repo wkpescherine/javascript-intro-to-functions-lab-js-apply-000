78cbebb14bad4cd35bc9bb808fd65780a23ac220
William Pescherine <wkpescherine@gmail.com>
2019-05-04 00:34:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string){
   var uppercase = "YES INDEED!";
   string.toUpperCase() === uppercase;
   
-  
+  return string
   var mixedcase = "I love you, Grandma.";
   string.toLowerCase() === mixedcase;
   return "I love you too";
